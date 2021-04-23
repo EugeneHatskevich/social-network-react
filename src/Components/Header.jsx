2ee48f98e39f928981671bdac573435820a32f37
EugeneHatskevich <71786969+EugeneHatskevich@users.noreply.github.com>
2021-04-23 15:22:34 +0300

    Test commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+    return(
+        <header className='header'>
+            <img
+                src='https://mpng.subpng.com/20180516/bhe/kisspng-computer-icons-red-eye-5afc404aa920b4.4199539915264809706928.jpg'
+                alt='Logo'/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div>
+                <img
+                    src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+                    alt='Image'/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
