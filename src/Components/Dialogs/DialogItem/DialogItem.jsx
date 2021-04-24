b8837a4e779ae6e2e4c93233111d62370480c928
EugeneHatskevich <71786969+EugeneHatskevich@users.noreply.github.com>
2021-04-25 00:10:41 +0300

    Step 29: My refactoring

--- src/Components/Dialogs/DialogItem/DialogItem.jsx
+++ src/Components/Dialogs/DialogItem/DialogItem.jsx
@@ -6,7 +6,8 @@ const DialogItem = (props) => {
     let path = '/dialogs/' + props.id
 
     return (
-        <div className={s.dialog + ' ' + s.active}>
+        <div className={s.dialog}>
+            <img src={props.avatar} alt='avatar'/>
             <NavLink to={path}>{props.name}</NavLink>
         </div>
     )
--- src/Components/Dialogs/Message/Message.jsx
+++ src/Components/Dialogs/Message/Message.jsx
@@ -1,9 +1,30 @@
 import s from "./Message.module.css";
 
 const Message = (props) => {
+
+    let myMessagesElements = props.message.myMessage.map((message) => {
+        return (
+            <div className={s.myMessage}>
+                {message.message}
+            </div>
+        )
+    })
+    let friendMessagesElements = props.message.friendMessage.map((message) => {
+        return (
+            <div className={s.friendMessage}>
+                {message.message}
+            </div>
+        )
+    })
+
     return (
         <div className={s.message}>
-            {props.message}
+            <div >
+                {myMessagesElements}
+            </div>
+            <div >
+                {friendMessagesElements}
+            </div>
         </div>
     )
 }
